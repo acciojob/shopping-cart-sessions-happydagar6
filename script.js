b1e072424dd10d7cfbffa1c628976c89883c831f
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-16 19:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // This is the boilerplate code given for you
 // You can modify this code
+
 // Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
@@ -11,27 +12,81 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearBtn = document.getElementById("clear-cart-btn");
+
+// Load cart from sessionStorage
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
-  products.forEach((product) => {
-    const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
-    productList.appendChild(li);
-  });
+  productList.innerHTML = "";
+
+  for (let i = 0; i < products.length; i++) {
+    productList.innerHTML += `
+      <li>
+        ${products[i].name} - $${products[i].price}
+        <button onclick="addToCart(${products[i].id})">
+          Add to Cart
+        </button>
+      </li>
+    `;
+  }
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = "";
+
+  for (let i = 0; i < cart.length; i++) {
+    cartList.innerHTML += `
+      <li>
+        ${cart[i].name} - $${cart[i].price}
+        <button onclick="removeFromCart(${cart[i].id})">
+          Remove
+        </button>
+      </li>
+    `;
+  }
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === productId) {
+      cart.push(products[i]);
+      break;
+    }
+  }
+
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id === productId) {
+      cart.splice(i, 1);
+      break;
+    }
+  }
+
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  cart = [];
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
+
+// Attach clear button
+clearBtn.onclick = clearCart;
 
 // Initial render
 renderProducts();
